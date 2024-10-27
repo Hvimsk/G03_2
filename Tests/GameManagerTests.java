f15739689a1460ed09797147feb39467cd66e2c6
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-27 12:16:12 +0100

    All inputs is being tested.

--- Tests/GameManagerTests.java
+++ Tests/GameManagerTests.java
@@ -77,4 +77,63 @@ public class GameManagerTests {
 
 
     }
+    @Test
+    public void TestAllFields(){
+        Locale enLocale = new Locale("en", "US");
+        LocalizationManager lm = new LocalizationManager(enLocale);
+
+        ArrayList<int[]> inputsToReachEvent = new ArrayList<>() {{
+            add(new int[]{1, 1});// 1250     player: 1
+
+            add(new int[]{1, 2});// 900     player: 2
+
+            add(new int[]{1, 2});// 1150     player: 1
+
+            add(new int[]{1, 2});// 800     player: 2
+
+            add(new int[]{1, 3});// 1250     player: 1
+
+            add(new int[]{1, 2});// 700     player: 2
+
+            add(new int[]{1, 4});// 1230     player: 1
+
+            add(new int[]{1, 2});// 600     player: 2
+
+            add(new int[]{1, 5});// 1930     player: 1
+
+            add(new int[]{1, 2});// 500     player: 2
+
+            add(new int[]{1, 6});// 1930     player: 1
+
+            add(new int[]{1, 2});// 400     player: 2
+
+            add(new int[]{2, 6});// 1860     player: 1
+
+            add(new int[]{1, 2});// 300     player: 2
+
+            add(new int[]{3, 6});// 1920     player: 1        double turn
+
+            add(new int[]{1, 2});// 200     player: 2
+
+            add(new int[]{4, 6});// 1840     player: 1        double turn
+            add(new int[]{5, 6});// 1790     player: 1        double turn
+
+            add(new int[]{1, 2});// 100     player: 2
+
+            add(new int[]{6, 6});// 2440     player: 1        double turn
+
+            add(new int[]{1, 2});// 0     player: 2
+
+            add(new int[]{6, 6});// 3090     player: 1        double turn
+
+        }};
+        var TestingRaffle = new MockRaffle(inputsToReachEvent);
+        var sut = new GameManager(TestingRaffle,lm);
+
+        Player playerWhoWon = sut.PlayGame();
+
+        assertEquals(1, playerWhoWon.playerPosition);
+
+
+    }
 }
