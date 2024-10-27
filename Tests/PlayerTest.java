e933b9d4354f5dfd15caa2c4016f608a4360fe09
hvimsk <Mathiasmagnussen@hotmail.dk>
2024-10-27 11:23:54 +0100

    MockRaffel

--- Tests/PlayerTest.java
+++ Tests/PlayerTest.java
@@ -1,5 +1,25 @@
 package Tests;
 
+import org.junit.jupiter.api.Test;
+import src.Player;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class PlayerTest {
+    @Test
+    void getScore() {
+        Player playerInstance = new Player(1);
+        int score = playerInstance.GetTotalScore();
+        assertEquals(0, score);
+    }
+
+    @Test
+    void setScore() {
+        Player playerInstance = new Player(1);
+        int[] testScore = new int[]{5,5};
+        playerInstance.SetScore(testScore);
+        int score = playerInstance.GetTotalScore();
+        assertEquals(10, score);
+    }
 
 }
--- Tests/Utility/MockRaffle.java
+++ Tests/Utility/MockRaffle.java
@@ -1,10 +1,22 @@
 package Tests.Utility;
 import src.interfaces.IRaffle;
 
+import java.util.ArrayList;
+
 public class MockRaffle implements IRaffle{
+    int currentIndOfListArray = 0;
+    private final ArrayList<int[]> ListOfInputs;
+    MockRaffle(ArrayList<int[]> inputs){
+        ListOfInputs = inputs;
+    }
 
     @Override
-    public void raffle() {
-
+    public int[] RaffleDices() {
+        if (currentIndOfListArray >= ListOfInputs.size()) {
+            throw new IllegalStateException("No more mock inputs available.");
+        }
+        int[] returnResults = ListOfInputs.get(currentIndOfListArray);
+        currentIndOfListArray++;
+        return returnResults;
     }
 }
