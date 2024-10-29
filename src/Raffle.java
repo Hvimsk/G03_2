a3b00fa16313b0e1cc5d8c5b167e9e165b084fab
oscaroscaar <oscar@gang.dk>
2024-10-29 16:45:10 +0100

    Implemented the Raffle class

--- src/Raffle.java
+++ src/Raffle.java
@@ -3,9 +3,19 @@ package src;
 import src.interfaces.IRaffle;
 
 public class Raffle implements IRaffle {
+    private final Die die1 = new Die();
+    private final Die die2 = new Die();
+
+    /**
+     * Raffle dices
+     *
+     * @return int[]
+     */
 
     @Override
     public int[] RaffleDices() {
-        return new int[0];
+        int faceValue1 = die1.Roll();
+        int faceValue2 = die2.Roll();
+        return new int[]{faceValue1, faceValue2};
     }
 }
