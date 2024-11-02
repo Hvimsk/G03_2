cec594b7d6cdb6aacae073f4791e7413ac57b9aa
friis <friis@post.com>
2024-11-03 00:29:54 +0100

    added menu to choose locale - standard is english

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 package src;
 
+import src.interfaces.IRaffle;
+
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-     //  var resourceBundle = ResourceBundle.getBundle("messages", Locale.ENGLISH);
-       LocalizationManager localizationManager = LocalizationManager.getInstance(Locale.ENGLISH);
-        System.out.println(localizationManager.getString("messages"));
+        var scanner = new Scanner(System.in);
+        System.out.println("[1]: English\n[2]: Dansk");
+        int input = scanner.nextInt();
+        LocalizationManager localizationManager = LocalizationManager.getInstance(Locale.ENGLISH);
+        if (input == 2){
+            Locale daLocale = new Locale("da", "DK");
+            localizationManager = LocalizationManager.getInstance(daLocale);
+        }else{
+            System.out.println("Invalid input");
+        }
+        Raffle ProductionRaffle = new Raffle(new Die(), new Die());
+        GameManager currentGame = new GameManager(ProductionRaffle, localizationManager);
+        int PlayerWhoWon = currentGame.PlayGame().playerPosition + 1;
+        System.out.println("Congratulation, Player: "+PlayerWhoWon +" Won");
     }
 }
